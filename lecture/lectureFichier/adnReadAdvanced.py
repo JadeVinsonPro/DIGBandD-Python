4d8de6fe09c7dd3f450b91352b6cc4bed67fc57c
Jade VINSON <jade.vinson23@gmail.com>
2022-10-25 12:11:15 +0200

    Amélioration TP - ADN

--- lecture/lectureFichier/adnReadAdvanced.py
+++ lecture/lectureFichier/adnReadAdvanced.py
@@ -2,26 +2,41 @@
 count = 0
 compteurC = 0
 compteurG = 0
-
+j = 0
+g = 0
+i = 0
+fichier = open("adn.txt").read()
+print(fichier)
 #On parcourt le fichier texte
-for i in (open("adn.txt").read()):
+for i in (fichier):
     #Compteur de la somme de caractères
-    count = count + 1
 
+    j = 0
+    print(str(fichier[count]))
     #Compteur du nombre de C
     if i == "C":
-        compteurC = compteurC + 1
+        j = count + 1
+        if str(fichier[g]) == "G":
+            compteurC = +1
+            print(compteurC)
+
 
     #Compteur du nombre de G
-    elif i == "G":
-        compteurG =compteurG + 1
+    if i == "G":
+        g = count + 1
+        print(str((fichier[g])))
+        if str(fichier[g]) == "C":
+            print("ok")
+            compteurG = compteurG +1
+            print(compteurG)
 
+    count = count + 1
 #Calcul du pourcentage parmi toute la ligne
-pourcentageC = round((compteurC * 100) / count)
-pourcentageG = round((compteurG * 100) / count)
+#pourcentageC = round((compteurCG * 100) / count)
+#pourcentageG = round((compteurGC * 100) / count)
 
 print("Le fichier contient", count, "caractères.")
 
-print("Nombres de C : ", compteurC, "\nSoit un pourcentage de :", pourcentageC)
-print("Nombres de G : ", compteurG, "\nSoit un pourcentage de :", pourcentageG)
+print("Nombres de CG : ", compteurC, "\nSoit un pourcentage de :", )
+print("Nombres de GC : ", compteurG, "\nSoit un pourcentage de :", )
 
